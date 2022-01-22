f260214d649c24154d9dde46c4cb21117f2c1e53
Zhoul <Zhoul@LZHOULT2.autodatacorp.org>
2022-01-22 13:06:27 -0500

    update logic of compare - ok

--- src/compareBS/MyStrComparable.java
+++ src/compareBS/MyStrComparable.java
@@ -12,16 +12,20 @@ public class MyStrComparable implements Comparator<String> {
 //     return (Integer.valueOf(o1)<Integer.valueOf(o2) ? -1 : (o1==o2 ? 0 : 1)); //Increase order
 //	 update  
 		int compareNum = o1.compareTo(o2);
+		System.out.println("compareNum="+compareNum);
 //	   if (o1.compareTo(o2)>0 ){ //original
-		if (compareNum ==2 ) {
+		if (compareNum ==2|| compareNum<0) {
 			//some of them do not match
 			System.out.println("compareNum==========================================================" + compareNum);
 			return 2;
-		} else if (o1.compareTo(o2) == 0) {
+		} else if (compareNum == 0) {
 			//All match
 			return 1;
-		} else {
-			//>2 Lots do not match
+		} else if (compareNum >= 2){
+			//>=2 Lots do not match
+			return -1;
+		}else {
+			//Some not match <0   
 			return -1;
 		}
 
